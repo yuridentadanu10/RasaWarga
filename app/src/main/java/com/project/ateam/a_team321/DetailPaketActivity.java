617735e2fba6c8651255dfd893981130139bbfaf
rifkiathar <athar.rosyadi@gmail.com>
2020-02-08 16:11:03 +0700

    [Rifki] add layout

---
+++ app/src/main/java/com/project/ateam/a_team321/DetailPaketActivity.java
@@ -0,0 +1,14 @@
+package com.project.ateam.a_team321;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailPaketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_paket);
+    }
+}
---
+++ app/src/main/java/com/project/ateam/a_team321/PesanPaketActivity.java
@@ -0,0 +1,14 @@
+package com.project.ateam.a_team321;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PesanPaketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesan_paket);
+    }
+}
