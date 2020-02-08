d75d3c32513c04814337ea784ee1873730155164
rifkiathar <athar.rosyadi@gmail.com>
2020-02-08 13:16:59 +0700

    [Rifki] add several layout

---
+++ app/src/main/java/com/project/ateam/a_team321/AgendesaDesaFragment.java
@@ -0,0 +1,31 @@
+package com.project.ateam.a_team321;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AgendesaDesaFragment extends Fragment {
+
+
+    public AgendesaDesaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agendesa_desa, container, false);
+    }
+
+}
