3a1905d22c8b4a76a21e0bbca2024e9aef46298c
rifkiathar <athar.rosyadi@gmail.com>
2020-02-09 08:36:46 +0700

    bayar

--- app/src/main/java/com/project/ateam/a_team321/BayarFragment.java
+++ app/src/main/java/com/project/ateam/a_team321/BayarFragment.java
@@ -2,16 +2,23 @@ package com.project.ateam.a_team321;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 public class BayarFragment extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvTotalBayar;
     Button btnTransfer, btnGopay, btnIndomaret, btnBayar;
     TextView tvKembali;
+    ImageView check1, check2, check3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +33,26 @@ public class BayarFragment extends AppCompatActivity implements View.OnClickList
         btnIndomaret = findViewById(R.id.btn_indomaret);
         btnBayar = findViewById(R.id.bayar);
 
+        check1 = findViewById(R.id.img_check);
+        check2 = findViewById(R.id.img_check2);
+        check3 = findViewById(R.id.img_check3);
+
 
         Bundle bundleFinal = getIntent().getExtras();
         String namaWarga=bundleFinal.getString("nama_warga");
         int harga=bundleFinal.getInt("totalHarga");
-        String namaDesa=
-        String provinsi=bundleFinal.getString("provinsi");
+        String namaDesa= bundleFinal.getString("nama_warga");
+
+        DecimalFormat kursIdr = (DecimalFormat) DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRp = new DecimalFormatSymbols();
+        formatRp.setCurrencySymbol("Rp");
+        formatRp.setMonetaryDecimalSeparator(',');
+        formatRp.setGroupingSeparator('.');
+        kursIdr.setDecimalFormatSymbols(formatRp);
+
+        String hargaIndo = kursIdr.format(Long.valueOf(harga));
+
+        tvTotalBayar.setText(hargaIndo);
 
 
         btnTransfer.setOnClickListener(this);
@@ -52,20 +73,31 @@ public class BayarFragment extends AppCompatActivity implements View.OnClickList
             }
 
             case  R.id.btn_transfer: {
-                //TODO: isi fungsi
+                check1.setVisibility(View.VISIBLE);
+                check2.setVisibility(View.GONE);
+                check3.setVisibility(View.GONE);
                 break;
             }
 
             case R.id.btn_gopay: {
-                //TODO
+                check1.setVisibility(View.GONE);
+                check2.setVisibility(View.VISIBLE);
+                check3.setVisibility(View.GONE);
                 break;
             }
             case R.id.btn_indomaret: {
-                //TODO
+                check1.setVisibility(View.GONE);
+                check2.setVisibility(View.GONE);
+                check3.setVisibility(View.VISIBLE);
                 break;
             }
             case R.id.bayar: {
-                //TODO
+                if (check1.getVisibility() == View.VISIBLE || check2.getVisibility() == View.VISIBLE || check3.getVisibility() == View.VISIBLE){
+                    Intent intent = new Intent(BayarFragment.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Kamu belum memilih metode pembayaran", Toast.LENGTH_SHORT).show();
+                }
             }
 
 
